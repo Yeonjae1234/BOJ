0316d6d9bb3ef601edbc97dd58b655036c58e632
JAE <yeoncom11@gmail.com>
2025-09-17 20:32:42 +0900

    Create [11726] 2xn 타일링.java

---
+++ Java/[11726] 2xn 타일링.java
@@ -0,0 +1,27 @@
+//  https://www.acmicpc.net/problem/11726
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+            arr[i]%=10007;
+        }
+
+        bw.write(String.valueOf(arr[n]));
+        bw.flush();
+
+    }
+}
