6a83a87248056dd33b7bf2ff0f9383ca4c5cefe7
JAE <yeoncom11@gmail.com>
2025-10-10 09:14:38 +0900

    Delete Python directory

--- Python/1018.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split( ))
-
-chess = []
-for i in range(n):
-    chess.append(input())
-
-for i in range(0,8):
-    for j in range(0,8):
-        if
--- Python/1436.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-count=0
-number=665
-while count<n:
-    number +=1
-    if "666" in str(number):
-        count +=1
-print(number)
